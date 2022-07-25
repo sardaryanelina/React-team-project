cfcc9fa07d4c10fcfa4d35a5a2dbd100ed0a6acc
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-07-25 01:05:45 -0400

    retouch

--- online-sunglass-store-app/src/pages/Contact.js
+++ online-sunglass-store-app/src/pages/Contact.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from '../images/Logo_withYellow.png';
 
 export default function Contact() {
-   
+
     return (
         <div className='container-fluid'>
-         <div className="row p-4">
+            <div className="row p-4">
                 <div className="pt-4 m-4">
-                <img  src={logo} alt="Logo of Maruel"  width="220"/>
-            </div>
-        
-            <p>Elina Sardaryan</p>
-            <p className="mt-4"><strong>Linkedin: 
-                </strong><a href='https://www.linkedin.com/in/elinasardaryan/' target={'_blank'}> https://www.linkedin.com/in/elinasardaryan/</a></p>
-                <p>Mahyar Kordmafi</p>
-                <p className="mt-4"><strong>Linkedin: 
-                </strong><a href='https://www.linkedin.com/in/mahyar-kordmafi-a0526410/' target={'_blank'}> https://www.linkedin.com/in/mahyar-kordmafi-a0526410/</a></p>
-                <p>Rouqi Wang</p>
-                <p className="mt-4"><strong>Linkedin: 
-                </strong><a href='https://www.linkedin.com/in/wang-ruoqi-b721901a4/' target={'_blank'}> https://www.linkedin.com/in/wang-ruoqi-b721901a4/</a></p>
-    
+                    <img src={logo} alt="Logo of Maruel" width="220" />
                 </div>
+                <p className="mt-4">Elina Sardaryan</p>
+                <p ><strong>Linkedin:
+                </strong><a href='https://www.linkedin.com/in/elinasardaryan/' target={'_blank'}> https://www.linkedin.com/in/elinasardaryan/</a></p>
+                <p className="mt-4">Mahyar Kordmafi</p>
+                <p ><strong>Linkedin:
+                </strong><a href='https://www.linkedin.com/in/mahyar-kordmafi-a0526410/' target={'_blank'}> https://www.linkedin.com/in/mahyar-kordmafi-a0526410/</a></p>
+                <p className="mt-4">Rouqi Wang</p>
+                <p ><strong>Linkedin:
+                </strong><a href='https://www.linkedin.com/in/wang-ruoqi-b721901a4/' target={'_blank'}> https://www.linkedin.com/in/wang-ruoqi-b721901a4/</a></p>
             </div>
+        </div>
     );
 }
