910eaf59b99a72074cda4fe006ae9d3c1b7753a3
rubywang130701 <109776578+rubywang130701@users.noreply.github.com>
2022-07-25 00:12:56 -0400

    Add files via upload
    Update file

--- online-sunglass-store-app/src/App.js
+++ online-sunglass-store-app/src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <NavBarComp />
+	  <div id="snipcart" data-api-key="NWMwZWNkZGMtZjU2ZS00YzM3LWFlZjYtMmM5Zjk0MWViZDcxNjM3Njg0OTY0ODg5NTk4MTM3" hidden>
+</div>
     </div>
   );
 }
