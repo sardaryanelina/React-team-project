953552d398e1bdd2b42da7ec7705ac74621d54f8
MahYarMafi <mahyar.mafi@gmail.com>
2022-07-24 23:57:41 -0400

    Contact.js added

--- online-sunglass-store-app/src/pages/Contact.js
+++ online-sunglass-store-app/src/pages/Contact.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import logo from '../images/logo.png';
+
+
+
 export default function Contact() {
     return (
+<<<<<<< HEAD
+        <div className='container-fluid'>
+        <div className="row p-4">
+            <div className="pt-4">
+                <img  src={logo} alt="Logo of Maruel"  width="220"/>
+            </div>
+            <p className="mt-5"> <strong>Program: </strong>MAP-06</p>
+            <p> <strong>Course: </strong>Client-side web programming</p>
+            <p>Elina Sardaryan</p>
+            <p className="mt-1"><strong>Linkedin: 
+                </strong><a href='https://www.linkedin.com/in/elinasardaryan/' target={'_blank'}> https://www.linkedin.com/in/elinasardaryan/</a></p>
+                <p>Mahyar Kordmafi</p>
+                <p className="mt-1"><strong>Linkedin: 
+                </strong><a href='https://www.linkedin.com/in/mahyar-kordmafi-a0526410/' target={'_blank'}> https://www.linkedin.com/in/mahyar-kordmafi-a0526410/</a></p>
+                <p>Rouqi Wang</p>
+                <p className="mt-1"><strong>Linkedin: 
+                </strong><a href='https://www.linkedin.com/in/mahyar-kordmafi-a0526410/' target={'_blank'}> https://www.linkedin.com/in/mahyar-kordmafi-a0526410/</a></p>
+  
+=======
         <div className='container-fluid  m-4"'>
             <div className="row p-4  m-4">
                 <div className="p-4 m-4">
                     Contact Us
                 </div>
             </div>
+>>>>>>> 51c7c58a472b151b238bb30869e9375a22528b2d
         </div>
+    </div>
     );
 }
